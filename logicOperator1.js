f3292109276b60169a1866df8e0ab8c62ad39e81
Acer <foramupadhyay@gmail.com>
2026-03-18 16:20:39 +0530

    Add second logic operator exercise

---
+++ logicOperator1.js
@@ -0,0 +1,6 @@
+let a=prompt("Enter any number");
+if(a%2==0 && a%3==0){
+    document.write("The number is divisible with 2 and 3");
+}else{
+    document.write("The number is not divisible with 2 and 3");
+}
